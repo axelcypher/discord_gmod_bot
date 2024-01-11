47848104bc6efc2f79f206eaf2104b56dadd71ec
axelcypher <60742523+axelcypher@users.noreply.github.com>
2024-01-11 18:09:59 +0100

    Update web.js

--- web.js
+++ web.js
@@ -93,8 +93,9 @@ bot.on('ready', () => {
   setInterval(() => {
 		gamedig.query({
         		type: 'garrysmod',
-              		host: $GAMESERVER_HOST,
-              		port: $GAMESERVER_QUERY_PORT
+              		host: GAMESERVER_HOST,
+              		port: GAMESERVER_QUERY_PORT,
+			requestRules: true
 		}).then((state) => {							 
 			bot.user.setActivity(`${state.players.length}/${state.maxplayers} online!`);
               		console.log(`${state.players.length}/${state.maxplayers} online!`);
